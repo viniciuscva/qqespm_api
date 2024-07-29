bfa89a9902f96206929db9a6721c76b25d2220a6
kelio-mv <keliojosue2@gmail.com>
2024-07-29 17:13:59 -0300

    Twice the space between points in the x axis to avoid overflow

--- static/js/spatial-pattern-drawing.js
+++ static/js/spatial-pattern-drawing.js
@@ -9,7 +9,7 @@ function generatePoints(numPoints, width, height, radius) {
 
   for (let i = 0; i < numPoints; i++) {
     const angle = ((2 * Math.PI) / numPoints) * i + Math.PI;
-    const x = centerX + radius * Math.cos(angle);
+    const x = centerX + 2 * radius * Math.cos(angle);
     const y = centerY + radius * Math.sin(angle);
     points.push({ x, y });
   }
