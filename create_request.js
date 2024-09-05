7c4c882e02a7312f44fc7a53252c8b67f944e951
Carlos Vinicius <minervinocva@gmail.com>
2024-09-05 18:21:23 -0300

    update

--- create_request.js
+++ create_request.js
@@ -52,21 +52,5 @@ fetch("http://127.0.0.1:5000/search", {
 
 
 
-curl -X POST -H "Content-Type: application/json" \
--d '{
-  "method": "qqespm",
-  "spatial_pattern": {
-    "vertices": [
-      {"id": 0, "keyword": "school"},
-      {"id": 1, "keyword": "bakery"},
-      {"id": 2, "keyword": "restaurant"}
-    ],
-    "edges": [
-      {"id": "0-1", "vi": 0, "vj": 1, "lij": 0, "uij": 1000, "sign": "-", "relation": null},
-      {"id": "1-2", "vi": 1, "vj": 2, "lij": 356, "uij": 2918, "sign": ">", "relation": null}
-    ]
-  }
-}' \
-http://127.0.0.1:5000/search
     
 
